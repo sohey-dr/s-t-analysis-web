18ba11966e69bd1a0dfc0d152da981b24384979d
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:04:59 +0900

    [ADD]ボタンがクリックされたら表示する文字列修正

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,24 @@ use components::button::Button;
 
 #[function_component(App)]
 fn app() -> Html {
+    let selected = use_state(|| "".to_string());
+    let onclick = {
+        let selected = selected.clone();
+        Callback::from(move |_| selected.set("Clicked!".to_string()))
+    };
+
     html! {
         <>
+            <div class="flex justify-center">
+                <h1>{ (*selected).clone() }</h1>
+            </div>
+            <button
+                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+                { onclick }
+            >
+                { "Click me" }
+            </button>
+
             // TODO: forなどで回して、Buttonを複数表示する
             <div class="flex justify-center my-5">
                 <Button content="S1" />
