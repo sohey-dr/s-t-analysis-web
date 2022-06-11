81d96f86b67dce6b57b32cb0011fa0926d5e71ac
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:42:19 +0900

    [ADD]selectedにcontentを渡す

--- src/components/Button.rs
+++ src/components/Button.rs
@@ -1,17 +1,23 @@
-use yew::{function_component, html, Properties};
+use yew::{function_component, html, Properties, Callback};
 
 #[derive(Properties, PartialEq)]
 pub struct ButtonProps {
     pub content: String,
-    pub onclick: yew::Callback<yew::MouseEvent>,
+    pub selected: yew::UseStateHandle<std::string::String>,
 }
 
 #[function_component(Button)]
 pub fn button(props: &ButtonProps) -> Html {
+    let content = props.content.clone();
+    let onclick = {
+        let selected = props.selected.clone();
+        Callback::from(move |_| selected.set(content.clone()))
+    };
+
     html! {
       <button
         class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-        onclick={ (*props).onclick.clone() }
+        { onclick }
       >
         { &props.content }
       </button>
