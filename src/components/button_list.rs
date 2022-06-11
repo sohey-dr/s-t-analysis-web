d0ba86a4ca1250d291a6d66e69b6e56b2a1e14f9
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:43:11 +0900

    [MODIFY]数修正

--- src/components/button_list.rs
+++ src/components/button_list.rs
@@ -12,13 +12,13 @@ pub fn button_list(props: &ButtonListProps) -> Html {
     html! {
       <>
         <div class="flex justify-center my-5">
-        { for (0..5).map(|i| html! {
+        { for (1..6).map(|i| html! {
                 <Button content={ format!("T{}", i) } selected={ (*props).selected.clone() } />
         }) }
         </div>
 
         <div class="flex justify-center my-5">
-        { for (0..5).map(|i| html! {
+        { for (1..6).map(|i| html! {
                 <Button content={ format!("S{}", i) } selected={ (*props).selected.clone() } />
         }) }
         </div>
