12d2dc9f058439938442c33444095228c65754d9
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:33:15 +0900

    [ADD]onclickが動作するようにする

--- src/components/Button.rs
+++ src/components/Button.rs
@@ -3,12 +3,16 @@ use yew::{function_component, html, Properties};
 #[derive(Properties, PartialEq)]
 pub struct ButtonProps {
     pub content: String,
+    pub onclick: yew::Callback<yew::MouseEvent>,
 }
 
 #[function_component(Button)]
 pub fn button(props: &ButtonProps) -> Html {
     html! {
-      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+      <button
+        class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        onclick={ (*props).onclick.clone() }
+      >
         { &props.content }
       </button>
     }
