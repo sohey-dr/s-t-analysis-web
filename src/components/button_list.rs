4670c7d81db026f8e60c9cc5b72093c9cb1b29d4
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:32:55 +0900

    [ADD]onclickを渡せるようにする

--- src/components/button_list.rs
+++ src/components/button_list.rs
@@ -1,19 +1,25 @@
-use yew::{function_component, html};
+use yew::{function_component, html, Properties};
+
 use crate::components::button::Button;
 
+#[derive(Properties, PartialEq)]
+pub struct ButtonListProps {
+    pub onclick: yew::Callback<yew::MouseEvent>,
+}
+
 #[function_component(ButtonList)]
-pub fn button_list() -> Html {
+pub fn button_list(props: &ButtonListProps) -> Html {
     html! {
       <>
         <div class="flex justify-center my-5">
         { for (0..5).map(|i| html! {
-                <Button content={ format!("T{}", i) } />
+                <Button content={ format!("T{}", i) } onclick={ (*props).onclick.clone() } />
         }) }
         </div>
 
         <div class="flex justify-center my-5">
         { for (0..5).map(|i| html! {
-                <Button content={ format!("S{}", i) } />
+                <Button content={ format!("S{}", i) } onclick={ (*props).onclick.clone() } />
         }) }
         </div>
       </>
