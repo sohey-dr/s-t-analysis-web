87d6738fc5786b45a399da7c4d7e36b09f7c3c47
soheyHashimoto <heyso1209@gmail.com>
2022-06-12 01:33:25 +0900

    clean

--- src/main.rs
+++ src/main.rs
@@ -18,14 +18,8 @@ fn app() -> Html {
             <div class="flex justify-center">
                 <h1>{ (*selected).clone() }</h1>
             </div>
-            <button
-                class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
-                { onclick }
-            >
-                { "Click me" }
-            </button>
 
-            <ButtonList />
+            <ButtonList onclick={ onclick } />
         </>
     }
 }
