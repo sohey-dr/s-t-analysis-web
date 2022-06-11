c3c16b582d9c6887c9a67ef5d29ef12cdb8de308
soheyHashimoto <heyso1209@gmail.com>
2022-06-11 20:58:55 +0900

    [ADD]ボタンを押しやすくする

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ fn app() -> Html {
     html! {
         <>
             // TODO: forなどで回して、Buttonを複数表示する
-            <div class="flex justify-center">
+            <div class="flex justify-center my-5">
                 <Button content="S1" />
                 <Button content="S2" />
                 <Button content="S3" />
                 <Button content="S4" />
             </div>
-            <div class="flex justify-center">
+            <div class="flex justify-center my-5">
                 <Button content="T1" />
                 <Button content="T2" />
                 <Button content="T3" />
