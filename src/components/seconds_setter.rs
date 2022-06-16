538085bf9d63450a1863b750e50ec29ccae3be6a
soheyHashimoto <heyso1209@gmail.com>
2022-06-16 14:26:54 +0900

    [ADD]onclickを追加

--- src/components/seconds_setter.rs
+++ src/components/seconds_setter.rs
@@ -21,6 +21,10 @@ pub fn seconds_setter(props: &SecondsSetterProps) -> Html {
             }
         })
     };
+    let onclick = {
+        Callback::from(move |_| {
+        })
+    };
 
     html! {
       <>
@@ -34,7 +38,10 @@ pub fn seconds_setter(props: &SecondsSetterProps) -> Html {
                 id="seconds"
                 class="text-center"
             />
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              { onclick }
+            >
                 { "秒でStart" }
             </button>
         </div>
